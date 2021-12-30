4e08281b7f6d9b8105ec09078eb872af51e8bb59
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2021-12-29 21:00:14 -0300

    API routes

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,17 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, useSession, signOut } from "next-auth/react";
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = false;
-
-    return isUserLoggedIn ? (
+    const {data:session} = useSession();
+    
+    return session ? (
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
         <FaGithub color="#04D361" />
             Gustavo Martins Ghisi
@@ -20,6 +22,7 @@ export function SignInButton() {
         <button
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
         <FaGithub color="#EBA417" />
             Sign in with Github
