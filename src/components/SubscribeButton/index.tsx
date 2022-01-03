b401e7650e39dd45391c86f90101da51ec5b4227
Gustavo Martins Ghisi <guga-ghisi@hotmail.com>
2022-01-03 20:07:49 -0300

    webhooks

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -10,24 +10,28 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data: session } = useSession()
 
-  async function handleSubscribe() {
-    if (!session) {
-      signIn("github");
-      return;
-    }
+    async function handleSubscribe() {
+      if (!session) {
+        signIn("github");
+        return;
+      }
 
-    try{
-      const response = await api.post('/subscribe')
-      const { sessionId} = response.data
-      const stripe = await getStripeJs
+      try{
+        const response = await api.post('/subscribe');
+        const { sessionId} = response.data;
+        const stripe = await getStripeJs();
 
-      await stripe.redirectToCheckout({sessionId})
-    } catch(err){
-      alert(err.message);
+        await stripe.redirectToCheckout({sessionId});
+      } catch(err){
+        alert(err.message);
+      }
     }
-  }
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
